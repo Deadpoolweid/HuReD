427e741be08591ef90ac6378341766ef06252a49
Alex_Green <deadpoolweid@yandex.ru>
2016-12-17 19:42:03 +0300

    Разработана предварительная версия интерфейса приложения

---
+++ Hured/Employees.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hured
+{
+    /// <summary>
+    /// Логика взаимодействия для Employees.xaml
+    /// </summary>
+    public partial class Employees : Window
+    {
+        public Employees()
+        {
+            InitializeComponent();
+        }
+
+        private void bAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            IsManipulationEnabled = false;
+            Employee w =new Employee();
+            w.ShowDialog();
+            IsManipulationEnabled = true;
+        }
+
+        private void BChange_OnClick(object sender, RoutedEventArgs e)
+        {
+            IsManipulationEnabled = false;
+            Employee w = new Employee();
+            w.ShowDialog();
+            IsManipulationEnabled = true;
+        }
+    }
+}
